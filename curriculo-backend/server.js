b5251282ce377f41b67fac805ff8d1a0e13b0256
danielLeiteSilva <daniel199257@gmail.com>
2022-08-05 18:28:37 -0300

    Altered backend

--- curriculo-backend/server.js
+++ curriculo-backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const Router = require('./src/Router')
 const cors = require('cors')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDoc= require('./swagger.json')
 require('dotenv').config()
 
 const app = express()
@@ -9,5 +11,6 @@ const port = process.env.PORT || 8080
 app.use(cors())
 app.use(express.json())
 app.use(Router)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
 app.listen(port, () => console.log(`Connected on port -> ${port}`))
